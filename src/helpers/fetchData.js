28d181c797ec4007ee1743e05f4eab3a5e79ace9
STEFAN CIOCIRLAN <stefan.ciocirlan@gammainnovation.it>
2022-07-04 10:17:52 +0200

    API  IP change

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -2,21 +2,14 @@ import { DEFAULT_POSITION } from "./constants";
 
 const fetchData = async (latlong = DEFAULT_POSITION) => {
    
-    const requestOptions = {
-        method: 'POST',
-        headers: {'Content-type':'application/json'},
-        body:JSON.stringify({lat:latlong?.lat, lon:latlong?.lng})
-    };
-
     try {
         if (latlong.lat) {
-            const response = await fetch(`http://localhost:8080/readApiData`, requestOptions);
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latlong.lat}&lon=${latlong.lng}&appid=76e52ab3dac16c2cdd001704cf9f85f6`)
             const resp = await response.json();
-            console.log(resp);
             return resp;
         }
     } catch (err) {
-        console.error(err)
+        console.err(err)
     }
 
 }
