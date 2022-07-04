fb8fc63eaf109ac5edcef2bf438c123968aec62c
STEFAN CIOCIRLAN <stefan.ciocirlan@gammainnovation.it>
2022-07-04 10:14:09 +0200

    Test

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -2,14 +2,21 @@ import { DEFAULT_POSITION } from "./constants";
 
 const fetchData = async (latlong = DEFAULT_POSITION) => {
    
+    const requestOptions = {
+        method: 'POST',
+        headers: {'Content-type':'application/json'},
+        body:JSON.stringify({lat:latlong?.lat, lon:latlong?.lng})
+    };
+
     try {
         if (latlong.lat) {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latlong.lat}&lon=${latlong.lng}&appid=76e52ab3dac16c2cdd001704cf9f85f6`)
+            const response = await fetch(`http://localhost:8080/readApiData`, requestOptions);
             const resp = await response.json();
+            console.log(resp);
             return resp;
         }
     } catch (err) {
-        console.err(err)
+        console.error(err)
     }
 
 }
